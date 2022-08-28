64cb5ae639a764192d81d4310105d3a0cd67068a
ming <13928124235@163.com>
2022-08-28 22:28:00 +0100

    EctopicLetterGrouping

---
+++ src/week2/unorderedMap/EctopicLetterGrouping.java
@@ -0,0 +1,34 @@
+package week2.unorderedMap;
+
+import java.util.*;
+
+public class EctopicLetterGrouping {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<String, List<String>>();
+            for (String str : strs) {
+                char[] array = str.toCharArray();
+                Arrays.sort(array);
+                String key = new String(array);
+                List<String> list = map.getOrDefault(key, new ArrayList<String>());
+                list.add(str);
+                map.put(key, list);
+            }
+            return new ArrayList<List<String>>(map.values());
+        }
+    }
+
+    //python3
+/*    class Solution:
+          def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+               mp = collections.defaultdict(list)
+
+               for st in strs:
+                   key = "".join(sorted(st))
+                   mp[key].append(st)
+
+                return list(mp.values())
+*/
+
+
+}
